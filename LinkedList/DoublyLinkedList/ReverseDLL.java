c472a74357abaa40cf41c4e33ef411b4c91a3013
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-12-25 19:33:15 +0530

    Add ReverseDLL class to reverse a doubly linked list
    Implemented a method to reverse a doubly linked list and added a main method for testing.

---
+++ LinkedList/DoublyLinkedList/ReverseDLL.java
@@ -0,0 +1,37 @@
+package LinkedList.DoublyLinkedList;
+
+public class ReverseDLL {
+
+    public static Node reverse(Node head)
+    {
+        if(head == null || head.next == null)
+        {
+            return head;
+        }
+        Node temp = null;
+        Node curr = head;
+        while(curr!=null)
+        {
+            temp = curr.prev;
+            curr.prev = curr.next;
+            curr.next = temp;
+            curr = curr.prev;
+        }
+        return temp.prev;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next=new Node(20);
+        head.next.next=new Node(30);
+        head.next.next.next=new Node(40);
+
+        System.out.print("Original LL : ");
+        Node.traversal(head);
+
+        head = reverse(head);
+
+        System.out.print("After reversing : ");
+        Node.traversal(head);
+    }
+}
