b1e0da3033cf5ed649c18e28a990ae38bc61d122
Arpita Sahoo <arpitasahoo795@gmail.com>
2025-12-29 18:28:56 +0530

    Create Node.java

---
+++ LeetCode/LinkedList/Easy/Node.java
@@ -0,0 +1,25 @@
+package LeetCode.LinkedList.Easy;
+
+public class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int value){
+        this.data=value;
+        this.next=null;
+        this.prev=null;
+    }
+
+    public static void traversal(Node head){
+        if(head==null){
+            System.out.println("Empty DLL ");
+            return;
+        }
+        Node curr=head;
+        while (curr!=null){
+            System.out.print(curr.data+" ");
+            curr=curr.next;
+        }
+    }
+}
