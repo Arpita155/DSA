9fcdf7ec2ac7dd6fabca76d93fc5b3b33ef8a7da
Arpita Sahoo <arpitasahoo795@gmail.com>
2026-01-19 16:23:26 +0530

    Print Fibonacci series up to N terms

---
+++ Recursion/ForPracticeImportantQuestions/BasicQuestions/FibonacciSeriesUptoNTerms.java
@@ -0,0 +1,19 @@
+package Recursion.ForPracticeImportantQuestions.BasicQuestions;
+
+public class FibonacciSeriesUptoNTerms {
+
+    public static int fib(int n){
+        if(n==0 || n==1){
+            return n;
+        }
+
+        return fib(n-1)+fib(n-2);
+    }
+    public static void main(String[] args) {
+        int n = 7;
+        System.out.print("Fibonacci series up to n terms : ");
+        for(int i=0;i<=n;i++){
+            System.out.print(fib(i)+" ");
+        }
+    }
+}
