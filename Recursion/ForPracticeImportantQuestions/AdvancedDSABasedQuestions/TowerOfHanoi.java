d5b89f9c3defe34faf8182f4f5e76deeb40c7630
Arpita Sahoo <arpitasahoo795@gmail.com>
2026-02-03 22:05:06 +0530

    Add print statement in TowerOfHanoi main method

--- Recursion/ForPracticeImportantQuestions/AdvancedDSABasedQuestions/TowerOfHanoi.java
+++ Recursion/ForPracticeImportantQuestions/AdvancedDSABasedQuestions/TowerOfHanoi.java
@@ -3,6 +3,6 @@ package Recursion.ForPracticeImportantQuestions.AdvancedDSAFocusedQuestions;
 public class TowerOfHanoi {
 
     public static void main(String[] args) {
-        
+        System.out.println("hi");
     }
 }
