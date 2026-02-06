3b3ffe134a4f0ee5a8df2304b628119cbc2a9f91
Arpita Sahoo <arpitasahoo795@gmail.com>
2026-02-06 22:10:04 +0530

    Implement Tower of Hanoi solution

--- Recursion/ForPracticeImportantQuestions/AdvancedDSABasedQuestions/TowerOfHanoi.java
+++ Recursion/ForPracticeImportantQuestions/AdvancedDSABasedQuestions/TowerOfHanoi.java
@@ -2,7 +2,16 @@ package Recursion.ForPracticeImportantQuestions.AdvancedDSAFocusedQuestions;
 
 public class TowerOfHanoi {
 
+    static void toh(int n, char src, char aux, char dest){
+        if(n==1){
+            System.out.println("Move disk 1 from "+src+" to "+dest);
+            return;
+        }
+        toh(n-1,src,dest,aux);
+        System.out.println("Move disk "+n+" from "+src+" to "+dest);
+    }
+    
     public static void main(String[] args) {
-        System.out.println("hi");
+        toh(3,'A','B','C');
     }
 }
