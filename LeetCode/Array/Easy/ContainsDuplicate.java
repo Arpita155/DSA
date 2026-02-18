36a4b23369bae755ea85b5b903f7a34d546c3cd5
Arpita Sahoo <arpitasahoo795@gmail.com>
2026-02-18 21:57:39 +0530

    Add ContainsDuplicate class to check for nearby duplicates

---
+++ LeetCode/Array/Easy/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+package LeetCode.Array.Easy;
+
+public class ContainsDuplicate {
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<=i+k && j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[]nums = {1,2,3,1};
+        int k = 3;
+        System.out.println("Is the array contains duplicate value : "+containsNearbyDuplicate(nums,k));
+    }
+}
